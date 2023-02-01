16e0969e56d471a4e1ca845f8193bb1d9cb7e194
LUk3VIT <lucasdiass061@gmail.com>
2023-02-01 14:00:16 -0300

    versão 0.2

--- assets/Js/navegacao.js
+++ assets/Js/navegacao.js
@@ -1,6 +1,26 @@
 const butao = document.querySelector('.header__button');
-const menu = document.querySelector('.nav')
+const menu = document.querySelector('.nav');
+const sobre = document.querySelector('.sobre');
+const formacao = document.querySelector('.formacao');
+const projetos = document.querySelector('.projetos');
+const tecnologia = document.querySelector('.tecnologia');
 
 butao.addEventListener('click', () => {
     menu.classList.toggle('nav--ativo');
+});
+
+sobre.addEventListener('click', () => {
+    menu.classList.remove('nav--ativo');
+});
+
+formacao.addEventListener('click', () => {
+    menu.classList.remove('nav--ativo');
+});
+
+projetos.addEventListener('click', () => {
+    menu.classList.remove('nav--ativo');
+});
+
+tecnologia.addEventListener('click', () => {
+    menu.classList.remove('nav--ativo');
 });
